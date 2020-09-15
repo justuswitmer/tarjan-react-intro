82f9b833cef39bb6be44d873799a4c6bc065f9c1
Justus Witmer <justus.witmer@gmail.com>
2020-09-15 10:19:20 -0500

    linked in React!

---
+++ src/PictureGallery.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class PictureGallery extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://picsum.photos/id/1020/200/200" alt="" />
+                <img src="https://picsum.photos/id/1024/200/200" alt="" />
+                <img src="https://picsum.photos/id/1018/200/200" alt="" />
+                <img src="https://picsum.photos/id/1003/200/200" alt="" />
+                <img src="https://picsum.photos/id/238/200/200" alt="" />
+                <img src="https://picsum.photos/id/237/200/200" alt="" />
+            </div>
+        );
+    }
+}
+
+
+export default PictureGallery;
