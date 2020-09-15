09da2c1740e2b2e63182271e300d8dfe24b0e702
Justus Witmer <justus.witmer@gmail.com>
2020-09-15 14:59:06 -0500

    added input fields

--- src/App.js
+++ src/App.js
@@ -3,21 +3,89 @@ import logo from './logo.svg';
 import './App.css';
 import PictureGallery from './PictureGallery';
 import ThingsToLearn from './ThingsToLearn';
-
+import getDate from './utils/getDate';
 
 // React component incanctation
 class App extends Component {
+
+  state = {
+    whatDayIsIt: 'Quinoa Day',
+    feeling: 'Dissapointed'
+  }
+
+  // When I click a button 
+  // I want this to change to
+  //whatDayIsIt: 'Cage Day',
+  //feeling: 'Clickalus'
+
+  //Save a `this`!
+  // Use arrow functions!
+  clickalicious = () => {
+    console.log('clickalicious was called');
+
+    // Always use this.setState()
+    this.setState({
+      feeling: 'Clickalus',
+      whatDayIsIt: 'Cage Day'
+    });
+  }
+
+  onFeelingChange = (event) => {
+    console.log('event.target', event.target);
+    this.setState({
+      feeling: event.target.value
+    });
+  }
+
+  onDayChange = (event) => {
+    console.log('event.target', event.target);
+    this.setState({
+      whatDayIsIt: event.target.value
+    });
+  }
+
+
+
+  // function
+  awesomeFunction() {
+    console.log('i have state', this.state);
+  }
+
+  dateAndFeel() {
+    return `${new Date().toLocaleString()} and I feel ${this.state.feeling}`;
+  }
+
   render() {
+    // Access state
+    console.log('App state is:', this.state);
+    console.log('this is', this);
+
+
+
+    this.awesomeFunction()
+    // Return some JSX
+    // JSX = 'Javascript eXtended'
     return (
       <div>
-        <h1>Happy React Day!</h1>
+        <h1>{this.state.feeling} {this.state.whatDayIsIt}!</h1>
+        <div>Today is {getDate()}</div>
+
+        <input type="text" placeholder="Feeling" onChange={this.onFeelingChange} />
+        <input type="text" placeholder="Day" onChange={this.onDayChange} />
+        <button onClick={this.clickalicious}>clickalicious</button>
         <h3>Things to learn about React:</h3>
         <ThingsToLearn />
         <h3>Check it out!</h3>
         <PictureGallery />
       </div>
+
     );
   }
+}
+
+
+function dateAndFeel() {
+  return `${new Date().toLocaleString()} and I feel ${this.state.feeling}`;
 }
 
 export default App;
--- src/PictureGallery.js
+++ src/PictureGallery.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 class PictureGallery extends Component {
+    state = {
+        photoWidth: 200,
+        photoHeight: 200
+    }
     render() {
         return (
             <div>
                 <img src="https://picsum.photos/id/1020/200/200" alt="" />
+                <img src={`https://picsum.photos/id/1020/${this.state.photoWidth}/${this.state.photoHeight}`} alt="" />
                 <img src="https://picsum.photos/id/1024/200/200" alt="" />
                 <img src="https://picsum.photos/id/1018/200/200" alt="" />
                 <img src="https://picsum.photos/id/1003/200/200" alt="" />
